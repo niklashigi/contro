2c7748a8c5e3b219ac30e0fc253de7ea32f9a6a9
Niklas Higi <niklas@shroudedcode.com>
2017-12-25 22:56:53 +0100

    feat(build): add banner to all output files

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,19 @@
 import typescript from 'rollup-plugin-typescript'
 
 const removeComments = () => ({
-  transform(code, id) {
-    code = code.replace(/\/\*[^*]+\*\/\s+/g, '')
-    return { code, map: { mappings: '' } }
+  transform(source, id) {
+    return source.replace(/\/\*[^*]+\*\/\s+/g, '')
+  }
+})
+
+const addBanner = () => ({
+  transformBundle(source) {
+    return `/*!
+ * Contro
+ * (c) 2017 Niklas Higi
+ * Released under the MIT License.
+ */
+` + source
   }
 })
 
@@ -14,6 +24,7 @@ export default {
       typescript: require('typescript'),
     }),
     removeComments(),
+    addBanner(),
   ],
   output: [
     {
