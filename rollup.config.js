3a94dd3a8c08712287be1139ed45592f9fbf6027
Niklas Higi <niklas@shroudedcode.com>
2017-12-25 21:45:50 +0100

    fix(build): remove comments from emitted files

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,19 @@
 import typescript from 'rollup-plugin-typescript'
 
+const removeComments = () => ({
+  transform(code, id) {
+    code = code.replace(/\/\*[^*]+\*\/\s+/g, '')
+    return { code, map: { mappings: '' } }
+  }
+})
+
 export default {
   input: 'src/index.ts',
   plugins: [
     typescript({
       typescript: require('typescript'),
-    })
+    }),
+    removeComments(),
   ],
   output: [
     {
