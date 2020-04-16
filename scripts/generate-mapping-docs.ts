c718f09cf429a6dc53973c1d78d4174bd740c3d7
Niklas Higi <niklas@shroudedcode.com>
2020-04-16 16:59:35 +0200

    chore: use native `fs` Promise methods instead of `fs-extra`

--- scripts/generate-mapping-docs.ts
+++ scripts/generate-mapping-docs.ts
@@ -1,4 +1,4 @@
-import * as fs from 'fs-extra'
+import { promises as fs } from 'fs'
 import * as path from 'path'
 import { buttonMap } from '../src/maps/gamepad'
 import { keyMap } from '../src/maps/keyboard'
